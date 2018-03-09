203203ac545304e96333b37271732f493e41b418
Deivis Largo <dlargo67@gmail.com>
2018-03-08 18:36:10 -0600

    first commit.

---
+++ src/main/java/com/indecision/springboot/indecisionproject/IndecisionProjectApplication.java
@@ -0,0 +1,12 @@
+package com.indecision.springboot.indecisionproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndecisionProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndecisionProjectApplication.class, args);
+	}
+}
